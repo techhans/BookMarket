f48b5271a9f8cdc4be8ddb440b98f5e22ea1ed1e
nicehans <nicehans@gmail.com>
2023-04-07 10:03:39 +0900

    test commit

--- src/main/java/com/springmvc/controller/Example06Controller.java
+++ src/main/java/com/springmvc/controller/Example06Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class Example06Controller {
- 
+  
 	@GetMapping("/exam06")
 	public String requestMethod(ModelMap model) {
 		model.addAttribute("data", "ModelMap 예제입니다");
